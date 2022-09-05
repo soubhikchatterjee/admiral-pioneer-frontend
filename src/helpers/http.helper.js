a4f86ed9407b63f7387daf4ace8fc6b2e056fb4f
Soubhik Chatterjee <soubhik@chatterjee.pw>
2022-09-05 09:32:55 +0530

    Update http.helper.js
    use env vars instead of hardcoding urls

--- src/helpers/http.helper.js
+++ src/helpers/http.helper.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const instance = axios.create({
-  baseURL: "//localhost:5000/api",
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
 });
 
 export default instance;
